42db74b1984ba569a75bb44e2b68be253d57efdb
mariosknl <mariosknl@outlook.com>
2024-10-19 10:59:13 +0300

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRouter from "./routes/products";
 
 const port = 3000;
+
 const app = express();
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
