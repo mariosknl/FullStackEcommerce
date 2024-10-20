8b0bbe87c19f0f9337c89bfad9c11bc531781fbb
mariosknl <mariosknl@outlook.com>
2024-10-20 13:03:23 +0300

    product item UI using Gluestack

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -5,15 +5,12 @@ import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-      />
-
-      <Button size="md" variant="solid" action="primary">
-        <ButtonText>Press me</ButtonText>
-      </Button>
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      contentContainerClassName="gap-2"
+      columnWrapperClassName="gap-2"
+      numColumns={2}
+    />
   );
 }
