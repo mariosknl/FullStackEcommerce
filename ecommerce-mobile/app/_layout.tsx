d3e17553d71770238023f8bcaaa2ff3ee872e594
mariosknl <mariosknl@outlook.com>
2024-10-20 19:55:58 +0300

    TanStack Query and API integration

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,14 +1,19 @@
 import { Stack } from "expo-router";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <GluestackUIProvider mode="light">
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Shop" }} />
-        <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
-      </Stack>
-    </GluestackUIProvider>
+    <QueryClientProvider client={client}>
+      <GluestackUIProvider mode="light">
+        <Stack>
+          <Stack.Screen name="index" options={{ title: "Shop" }} />
+          <Stack.Screen name="product/[id]" options={{ title: "Product" }} />
+        </Stack>
+      </GluestackUIProvider>
+    </QueryClientProvider>
   );
 }
--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,17 +1,35 @@
-import { View, Text, FlatList, useWindowDimensions } from "react-native";
-import products from "../assets/products.json";
+import { View, FlatList, ActivityIndicator } from "react-native";
 import ProductListItem from "../components/ProductListItem";
-import { Button, ButtonText } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
 import { useBreakpointValue } from "@/components/ui/utils/use-break-point-value";
+import { listProducts } from "@/api/products";
+import { useQuery } from "@tanstack/react-query";
 
 export default function HomeScreen() {
-  const { width } = useWindowDimensions();
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: listProducts,
+  });
 
   const numColumns = useBreakpointValue({
     default: 2,
     sm: 3,
     xl: 4,
   });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Error fetching products</Text>;
+  }
+
+  console.log("data");
 
   return (
     <FlatList
